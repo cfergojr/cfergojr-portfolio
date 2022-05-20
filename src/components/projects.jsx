5c5b67e12ef27e51cdceb92537d0868828f78d54
Christopher Fergo <cfergojr@gmail.com>
2022-05-19 22:49:31 -0400

    ADA fixes

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -19,12 +19,12 @@ export default class Projects extends Component {
               <div className="startup-grid">
                 <div className="columns is-multiline level">
                   <div className="column is-12-mobile is-half-tablet is-one-third-desktop level-item">
-                    <div className="box"><img className="logo" src="img/projects/python.svg" />
-                      <p>A plethora of Python projects I've worked on including games, file duplicator, and data fetcher.</p><a className="button is-primary is-outlined is-rounded" href="https://github.com/cfergojr/Python-projects" target="_blank">View Repo</a>
+                    <div className="box"><img className="logo" alt="python logo" src="img/projects/python.svg" />
+                      <p>A plethora of Python projects I've worked on including games, file duplicator, and data fetcher.</p><a className="button is-primary is-outlined is-rounded" href="https://github.com/cfergojr/Python-projects" target="_blank" rel="noreferrer">View Repo</a>
                     </div>
                   </div>
                   <div className="column is-12-mobile is-half-tablet is-one-third-desktop level-item">
-                    <div className="box"><img className="logo" src="img/projects/typing-test.png" />
+                    <div className="box"><img className="logo" alt="keyboard" src="img/projects/typing-test.png" />
                       <p>Who doesn't love a good typing test? This was built using vanilla JavaScript, along with some CSS and HTML.</p><a className="button is-primary is-outlined is-rounded mr-3" href="/typing-test.html">Play Game</a><a className="button is-primary is-outlined is-rounded" href="https://github.com/cfergojr/js-typing-test">View Repo</a>
                     </div>
                   </div>
@@ -34,18 +34,18 @@ export default class Projects extends Component {
                     </div>
                   </div>
                   <div className="column is-12-mobile is-half-tablet is-one-third-desktop level-item">
-                    <div className="box"><img className="logo" style={{height: '75px'}} src="img/projects/drumming.png" />
-                      <p>Find me behind my drum kit, performing various covers of popular songs.</p><a className="button is-primary is-outlined is-rounded" href="https://www.youtube.com/playlist?list=PLYm5qfbqopB22Oq6ars8Qp5-ZgVE8lTFi" target="_blank">Youtube Playlist</a>
+                    <div className="box"><img className="logo" alt="drums" style={{height: '75px'}} src="img/projects/drumming.png" />
+                      <p>Find me behind my drum kit, performing various covers of popular songs.</p><a className="button is-primary is-outlined is-rounded" href="https://www.youtube.com/playlist?list=PLYm5qfbqopB22Oq6ars8Qp5-ZgVE8lTFi" target="_blank" rel="noreferrer">Youtube Playlist</a>
                     </div>
                   </div>
                   <div className="column is-12-mobile is-half-tablet is-one-third-desktop level-item">     
-                    <div className="box"><img className="logo" src="img/projects/retropie.png" />
+                    <div className="box"><img className="logo" alt="Retropie logo" src="img/projects/retropie.png" />
                       <p>Built a fully working arcade cabinet using the Raspberry Pi 4b, Retropie OS, &amp; custom hardware.</p><a className="button is-primary is-outlined is-rounded" href="https://photos.google.com/album/AF1QipNVSIViPfVrvOsL1tX2nfjuhowpl9Q_rzbgcZwN">Retropie Arcade Gallery</a>
                     </div>
                   </div>
                   <div className="column is-12-mobile is-half-tablet is-one-third-desktop level-item">
-                    <div className="box"><img className="logo" src="img/projects/goodreads.svg" />
-                      <p>Fellow bookworm? Add me on Goodreads and shoot me some recommendations.</p><a className="button is-primary is-outlined is-rounded" href="https://www.goodreads.com/user/show/17724266-chris-fergo" target="_blank">Goodreads Bookshelf</a>
+                    <div className="box"><img className="logo" alt="Goodreads logo" src="img/projects/goodreads.svg" />
+                      <p>Fellow bookworm? Add me on Goodreads and shoot me some recommendations.</p><a className="button is-primary is-outlined is-rounded" href="https://www.goodreads.com/user/show/17724266-chris-fergo" target="_blank" rel="noreferrer">Goodreads Bookshelf</a>
                     </div>
                   </div>
                 </div>
