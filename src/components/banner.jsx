35cc557e6f736e5217c0488102874315e1bec385
Christopher Fergo <cfergojr@gmail.com>
2022-06-01 19:16:46 -0400

    Update banner.jsx

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -8,7 +8,7 @@ export default class Navigation extends Component {
           <div className="container">
             <div className="columns is-centered">
               <div className="column">
-                <span className="title is-4 pr-5"><a href="https://marchforourlives.com/" rel="noreferrer" target="_blank"><img src="/img/logos/March-For-Our-Lives.png" className="pr-2"></img>March For Our Lives</a></span>
+                <span className="title is-uppercase is-4 pr-5"><a href="https://marchforourlives.com/" rel="noreferrer" target="_blank"><img src="/img/logos/March-For-Our-Lives.png" className="pr-2"></img>March For Our Lives</a></span>
                 <span className="subtitle is-6">Help end senseless gun violence in America.</span>
                 </div>
             </div>
