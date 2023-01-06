6c4e2ec462d1216a741a0cbedf502a5d814ba955
Christopher Fergo <cfergojr@gmail.com>
2023-01-05 23:42:52 -0500

    Updated Footer and Navbar

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
 export default class Footer extends Component {
+  getYear() {
+    return new Date().getFullYear();
+  }
     render() {
       return (
         <footer className="section is-primary is-small has-text-centered">
@@ -13,7 +16,8 @@ export default class Footer extends Component {
             <div className="social-icons">
               <p className="field"><a className="button is-medium" title="Github link" href="https://www.github.com/cfergojr" target="_blank" rel="noreferrer"><span className="icon is-small"><i className="fab fa-github fa-fw" /></span></a><a className="button is-medium" href="https://dribbble.com/cfergojr" title="Dribbble Link" target="_blank" rel="noreferrer"><span className="icon is-small"><i className="fab fa-dribbble fa-fw" /></span></a><a className="button is-medium" title="LinkedIn Link" href="https://www.linkedin.com/in/cfergojr" target="_blank" rel="noreferrer"><span className="icon is-small"><i className="fab fa-linkedin-in fa-fw" /></span></a><a className="button is-medium" title="email link" href="mailto:cfergojr@gmail.com"><span className="icon is-small"><i className="far fa-envelope fa-fw" /></span></a></p>
             </div>
-            <div className="copyright">Handcrafted by me<span className="icon has-white-text" style={{verticalAlign: 'middle'}}><i className="far fa-copyright" /></span><span id="year">2022</span></div>
+            <div className="copyright">Handcrafted by me<span className="icon has-white-text" style={{verticalAlign: 'middle'}}><i className="far fa-copyright" /></span>
+            <span>{this.getYear()}</span></div>
           </div>
         </footer>
       );
